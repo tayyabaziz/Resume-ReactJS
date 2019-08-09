c74eeb369a889fe260887430e5236be88492944f
TAYYAB-PC\Tayyab <tayyabaziz5@gmail.com>
2019-08-09 09:37:38 +0500

    Updates

---
+++ src/components/Progress.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Progress(data) {
+  return (
+    <React.Fragment>
+      {/* Progress*/}
+      <div className="progress">
+        <div className="progress-bar" role="progressbar" aria-valuenow={data.progress_value} aria-valuemin="0" aria-valuemax="100">
+          <div className="progress-text"><span>{data.progress_text}</span><span>{data.progress_value}%</span></div>
+        </div>
+        <div className="progress-text"><span>{data.progress_text}</span></div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Progress;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Education from './Education';
-import Experience from './Experience';
+import Progress from './Progress';
 
 function Skills(data) {
     return (
@@ -9,76 +8,22 @@ function Skills(data) {
             <div className="box-inner box-inner--rounded">
                 <div className="row">
                     <div className="col-12 col-lg-6">
-                        <h2 className="title title--h3">Design Skills</h2>
+                        <h2 className="title title--h3">Skills</h2>
                         <div className="box box__second">
-                            {/* Progress*/}
-                            <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
-                                    <div className="progress-text"><span>Web Design</span><span>80%</span></div>
-                                </div>
-                                <div className="progress-text"><span>Web Design</span></div>
-                            </div>
-
-                            {/* Progress*/}
-                            <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                                    <div className="progress-text"><span>Graphic Design</span><span>75%</span></div>
-                                </div>
-                                <div className="progress-text"><span>Graphic Design</span></div>
-                            </div>
-
-                            {/* Progress*/}
-                            <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                                    <div className="progress-text"><span>Photoshop</span><span>90%</span></div>
-                                </div>
-                                <div className="progress-text"><span>Photoshop</span></div>
-                            </div>
-
-                            {/* Progress*/}
-                            <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-                                    <div className="progress-text"><span>Illustrator</span><span>50%</span></div>
-                                </div>
-                                <div className="progress-text"><span>Illustrator</span></div>
-                            </div>
+                            <Progress progress_value="70" progress_text="PHP - 5 years experience" />
+                            <Progress progress_value="65" progress_text="Wordpress - 4 years experience" />
+                            <Progress progress_value="25" progress_text="Photoshop - 1 year experience" />
+                            <Progress progress_value="90" progress_text="HTML/CSS - 5 years experience" />
                         </div>
                     </div>
 
                     <div className="col-12 col-lg-6 mt-4 mt-lg-0">
-                        <h2 className="title title--h3">Coding Skills</h2>
+                        <h2 className="title title--h3">&nbsp;</h2>
                         <div className="box box__second">
-                            {/* Progress*/}
-                            <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
-                                    <div className="progress-text"><span>WordPress</span><span>70%</span></div>
-                                </div>
-                                <div className="progress-text"><span>WordPress</span></div>
-                            </div>
-
-                            {/* Progress*/}
-                            <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
-                                    <div className="progress-text"><span>Javascript</span><span>70%</span></div>
-                                </div>
-                                <div className="progress-text"><span>Javascript</span></div>
-                            </div>
-
-                            {/* Progress*/}
-                            <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                                    <div className="progress-text"><span>HTML</span><span>100%</span></div>
-                                </div>
-                                <div className="progress-text"><span>HTML</span></div>
-                            </div>
-
-                            {/* Progress*/}
-                            <div className="progress">
-                                <div className="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100">
-                                    <div className="progress-text"><span>PHP</span><span>30%</span></div>
-                                </div>
-                                <div className="progress-text"><span>PHP</span></div>
-                            </div>
+                            <Progress progress_value="70" progress_text="JavaScript - 5 years experience" />
+                            <Progress progress_value="40" progress_text="NodeJS - less than a year experience" />
+                            <Progress progress_value="85" progress_text="jQuery/AJAX - 5 years experience" />
+                            <Progress progress_value="35" progress_text="Android - 1 year experience" />
                         </div>
                     </div>
                 </div>
