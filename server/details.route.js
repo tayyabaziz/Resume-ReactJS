47c24c231d07eb7ac878b99435c82398b352009b
tayyabaziz <tayyabaziz5@gmail.com>
2019-08-21 18:11:22 +0500

    no message

--- server/details.route.js
+++ server/details.route.js
@@ -2,6 +2,20 @@ import express from "express";
 import detailModel from "./detail.model";
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    detailModel.find().exec((err, data) => {
+        if (err) {
+            res.status(500).json("Error Occured");
+        } else {
+            if (data === null || data.length === 0) {
+                res.status(404).json("Details Not Found");
+            } else {
+                res.status(200).json(data);
+            }
+        }
+    });
+});
+
 router.get("/:type", (req, res) => {
     detailModel.findOne().where("type", req.params.type).exec((err, data) => {
         if (err) {
