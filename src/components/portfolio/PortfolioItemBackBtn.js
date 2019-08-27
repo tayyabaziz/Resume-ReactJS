438dd093bbb7e5943206f1f71324942940e1eaa0
TAYYAB-PC\Tayyab <tayyabaziz5@gmail.com>
2019-08-28 04:18:05 +0500

    Stylesheet Updated

--- src/components/portfolio/PortfolioItemBackBtn.js
+++ src/components/portfolio/PortfolioItemBackBtn.js
@@ -1,10 +1,9 @@
 import React from "react";
-import iconBack from "../../assets/icons/icon-back.svg";
 
 function PortfolioItemBackBtn(data) {
     return (
         <div onClick={() => data.history.push("/portfolio/")} className="circle-back">
-            <img src={iconBack} alt="" />
+            <i className="fa fa-arrow-left fa-2x" style={{ color: "white" }}></i>
         </div>
     );
 }
