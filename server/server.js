5f7996ab292aeb49f455ec005ecd90e32dc9bcd5
tayyabaziz <tayyabaziz5@gmail.com>
2019-10-24 11:48:04 +0500

    Update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== "production") {
     dotenv.config();
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080 || 3000;
 const app = express();
 app.use(compression());
 
