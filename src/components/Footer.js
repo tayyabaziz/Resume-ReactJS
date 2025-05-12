6fc9a0938565c1617e36f31440fbee284a7b4118
tayyabaziz <tayyabaziz5@gmail.com>
2025-05-13 01:12:15 +0500

    no message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer(data) {
             {/*Footer*/}
             <footer className="box my-4 footer text-dark">
                 © {(new Date().getFullYear())} Tayyab Aziz | 
-                <a className="ml-1 btn-link" style={{color:rgb(0, 49, 255)}} href="/sitemap/">Site map</a>
+                <a className="ml-1 btn-link" style="color:#0031ff" href="/sitemap/">Site map</a>
             </footer>
         </React.Fragment>
     );
