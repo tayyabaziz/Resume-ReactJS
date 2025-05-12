629e3e5a1c9d2a163bf1c89248320ad6978b156e
tayyabaziz <tayyabaziz5@gmail.com>
2025-05-13 01:30:16 +0500

    no message

--- src/components/contactbox/ContactBoxAvatar.js
+++ src/components/contactbox/ContactBoxAvatar.js
@@ -5,7 +5,7 @@ function ContactBoxAvatar(data) {
   return (
     <svg className="avatar avatar--180" viewBox="0 0 188 188">
       <g className="avatar__box">
-        <NavLink ariaLabel="Tayyab Aziz" to="/">
+        <NavLink aria-label="Tayyab Aziz" to="/">
           <image href={data.profileImage} height="100%" width="100%" />
         </NavLink>
       </g>
